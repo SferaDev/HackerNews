7e6e8605470e154e94165e596ab03ad72365f6a7
Alexis Rico <sferadev@gmail.com>
2018-04-09 12:57:44 +0200

    Add user schema model

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const userSchema = new schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Before save, hash the stored password to database
+userSchema.pre('save', function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = bcrypt.hashSync(this.password, 10);
+    next();
+});
+
+// Compare Password async with a callback(error, isMatch)
+userSchema.methods.comparePassword = function(candidatePassword) {
+    return bcrypt.compareSync(candidatePassword, this.password);
+};
+
+// Export Beneficiary model as module
+module.exports = mongoose.model('Users', userSchema);
