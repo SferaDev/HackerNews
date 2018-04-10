7f5fb7e2ee9eda7ed1fa76c3b10ac90c7940fd50
Alexis Rico <sferadev@gmail.com>
2018-04-10 19:07:05 +0200

    Produce and consume tokens

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,7 @@
+const jwt = require('jsonwebtoken');
 const user = require('../models/user');
+
+const TOKEN_SECRET = process.env.TOKEN_SECRET || 'randomTokenSecret';
 
 export function getUser(userId, next) {
     user.findOne({
@@ -21,7 +24,10 @@ export function loginUser(username, password, next) {
         } else if (user == null) {
             next(null, null);
         } else if (user.comparePassword(password))
-            next(null, user._id);
+            next(null, jwt.sign({
+                userId: user._id,
+                username: user.username
+            }, TOKEN_SECRET, { expiresIn: 60 * 60 * 24 * 365 }));
     })
 }
 
@@ -33,6 +39,18 @@ export function registerUser(username, password, next) {
         if (err) {
             console.error(err);
             next(err);
-        } else next(null, user._id);
+        } else next(null, jwt.sign({
+            userId: user._id,
+            username: user.username
+        }, TOKEN_SECRET, { expiresIn: 60 * 60 * 24 * 365 }));
+    });
+}
+
+export function validateUser(token, next) {
+    jwt.verify(token, TOKEN_SECRET, function(err, decoded) {
+        if (err) console.error(err);
+        else {
+            next(decoded);
+        }
     });
 }
