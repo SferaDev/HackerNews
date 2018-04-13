944e8ee81e12e2bf0356c86eaeb1193b2c53b576
Alexis Rico <sferadev@gmail.com>
2018-04-13 11:34:48 +0200

    Example using session storage

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res){
+    if(req.session.page_views){
+        req.session.page_views++;
+        res.send("You visited this page " + req.session.page_views + " times");
+    } else {
+        req.session.page_views = 1;
+        res.send("Welcome to this page for the first time!");
+    }
 });
 
 module.exports = router;
