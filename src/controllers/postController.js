d3f137e187d7a074d003a6e6d17d8c91a81001a8
Alexis Rico <sferadev@gmail.com>
2018-04-16 22:55:19 +0200

    feature/threads: Create threads endpoint

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,4 +1,5 @@
 import {askSchema, postSchema, urlSchema} from "../models/post";
+import {getUserByUsername} from "./userController";
 
 export function insertUrlPost(userId, title, url) {
     new urlSchema({
@@ -34,9 +35,20 @@ export function getPostById(postId, next) {
     })
 }
 
-export function getPostByTld(postTld, next) {
+export function getPostsByTld(postTld, next) {
     postSchema.find({tld: postTld}).populate('owner').exec(function (err, elements) {
         if (err) next('[]');
         else next(elements);
+    })
+}
+
+export function getPostsByOwner(username, next) {
+    getUserByUsername(username, function (user) {
+        if (user !== null) {
+            postSchema.find({owner: user._id}).populate('owner').exec(function (err, elements) {
+                if (err) next('[]');
+                else next(elements);
+            })
+        } else next('[]');
     })
 }
