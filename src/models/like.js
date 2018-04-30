51ad4bbc85147b13cf40f935c5646731467e2ce9
Alexis Rico <sferadev@gmail.com>
2018-04-30 08:56:06 +0200

    feature/vote: Add user karma feature

--- src/models/like.js
+++ src/models/like.js
@@ -19,27 +19,30 @@ const likeSchema = new mongoose.Schema({
 
 // Before save, increment like count
 likeSchema.pre('save', function (next) {
-    if (!this.isNew) return next();
-    postModel.findOne({
-        _id: this.post
-    }, function (err, post) {
-        if (err) console.error(err);
-        post.totalLikes += 1;
-        post.save();
-        next();
-    });
+    this.wasNew = this.isNew;
+    next();
+});
+
+likeSchema.post('save', function (doc) {
+    if (!this.wasNew) return;
+    incrementLikeAndKarma(doc, 1);
 });
 
 // Before remove, increment like count
-likeSchema.pre('remove', function (next) {
-    postModel.findOne({
-        _id: this.post
-    }, function (err, post) {
-        if (err) console.error(err);
-        post.totalLikes -= 1;
-        post.save();
-        next();
-    });
+likeSchema.post('remove', function (doc) {
+    incrementLikeAndKarma(doc, -1);
 });
+
+function incrementLikeAndKarma(doc, incr) {
+    postModel.findOne({
+        _id: doc.post
+    }).populate('owner').exec(function (err, post) {
+        if (err || post === null) return console.error(err);
+        post.totalLikes += incr;
+        post.save();
+        post.owner.karma += incr;
+        post.owner.save();
+    });
+}
 
 export const likeModel = mongoose.model('Like', likeSchema);
