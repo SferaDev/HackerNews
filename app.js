3705cc37fd5f7b0504300798c8f18c9f4e784a51
Alexis Rico <SferaDev@users.noreply.github.com>
2018-05-08 22:32:04 +0200

    fix/404: Move logic to routers

--- app.js
+++ app.js
@@ -82,19 +82,3 @@ if (process.env.GITHUB_CLIENT_ID) {
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
-
-// Detect errors and forward to 404
-app.use(function (req, res, next) {
-    next(createError(404));
-});
-
-// Error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
