3b54559d4cfc1637e6264e10f8de93a8cf258422
Alexis Rico <SferaDev@users.noreply.github.com>
2018-05-08 22:33:58 +0200

    Add html 404 to indexRouter

--- src/routes/index.js
+++ src/routes/index.js
@@ -36,3 +36,19 @@ routes.forEach(function (doc) {
         });
     }
 });
+
+// Detect errors and forward to 404
+indexRouter.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// Error handler
+indexRouter.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
