b8f752d99b6dc3640a0a29a7dd32a27c055048ec
Alexis Rico <sferadev@gmail.com>
2018-05-12 19:17:44 +0200

    fix/unique: Make base onCreate to be more robust

--- src/routes/api/base.js
+++ src/routes/api/base.js
@@ -12,28 +12,35 @@ export const modelGetAll = function (model, query, req, res) {
 };
 
 export const modelCreate = function (model, req, res) {
-    let attributes = {};
-    let findParameters = {};
-    findParameters[model.identifier()] = req.body[model.identifier().replace('_', '')];
-
-    let requiredProperties = propertyFinder(model, 'required');
-    for (let i = 0; i < requiredProperties.length; ++i) {
-        if (req.body[requiredProperties[i]]) attributes[requiredProperties[i]] = req.body[requiredProperties[i]];
-        else return messageCallback(res, httpCodes.STATUS_BAD_REQUEST, 'Missing parameter ' + requiredProperties[i]);
+    let findParameters = [{[model.identifier()]: req.body[model.identifier()]}];
+    let uniqueProperties = propertyFinder(model, 'unique');
+    for (let i = 0; i < uniqueProperties.length; ++i) {
+        if (req.body[uniqueProperties[i]]) {
+            findParameters.push({[uniqueProperties[i]]: req.body[uniqueProperties[i]]});
+        }
     }
 
-    let editableProperties = [...new Set([...propertyFinder(model, 'editable'), ...propertyFinder(model, 'final')])];
-    for (let i = 0; i < editableProperties.length; ++i) {
-        if (req.body[editableProperties[i]]) attributes[editableProperties[i]] = req.body[editableProperties[i]];
-    }
-
-    model.count(findParameters, function (err, count) {
+    model.count({$or: findParameters}, function (err, count) {
         if (err) return messageCallback(res, httpCodes.STATUS_SERVER_ERROR, err);
         if (count > 0) return messageCallback(res, httpCodes.STATUS_CONFLICT, 'Element already exists');
+
+        let attributes = {};
+
+        let requiredProperties = propertyFinder(model, 'required');
+        for (let i = 0; i < requiredProperties.length; ++i) {
+            if (req.body[requiredProperties[i]]) attributes[requiredProperties[i]] = req.body[requiredProperties[i]];
+            else return messageCallback(res, httpCodes.STATUS_BAD_REQUEST, 'Missing parameter ' + requiredProperties[i]);
+        }
+
+        let editableProperties = [...new Set([...propertyFinder(model, 'editable'), ...propertyFinder(model, 'final')])];
+        for (let i = 0; i < editableProperties.length; ++i) {
+            if (req.body[editableProperties[i]]) attributes[editableProperties[i]] = req.body[editableProperties[i]];
+        }
+
         model.create(attributes, function (err2, element) {
             if (err2) return messageCallback(res, httpCodes.STATUS_BAD_REQUEST, err2);
             return messageCallback(res, httpCodes.STATUS_CREATED, 'Element created (invalid attributes discarded)');
-        })
+        });
     });
 };
 
