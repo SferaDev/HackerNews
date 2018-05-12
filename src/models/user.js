0122f65f8021e41c3894163c17bbcf43a0144f10
Alexis Rico <sferadev@gmail.com>
2018-05-12 17:08:31 +0200

    Revert "fix/user: Remove findOneOrCreate"
    This reverts commit 9f62fce9eb48ce97328ed2e0ae3c07916875c588.

--- src/models/user.js
+++ src/models/user.js
@@ -76,6 +76,15 @@ userSchema.methods.comparePassword = function (candidatePassword) {
     return bcrypt.compareSync(candidatePassword, this.password);
 };
 
+userSchema.statics.findOneOrCreate = function findOneOrCreate(condition, callback) {
+    const self = this;
+    self.findOne(condition, (err, result) => {
+        return result ? callback(err, result) : self.create(condition, (err, result) => {
+            return callback(err, result)
+        })
+    })
+};
+
 userSchema.statics.identifier = () => 'username';
 
 userSchema.methods.canEdit = function (userId) {
